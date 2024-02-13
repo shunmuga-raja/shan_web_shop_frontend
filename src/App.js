dc11313eff00c60a8a1199ef7385701958f71dd4
shammurajas <shunmuga@anjamerica.com>
2024-02-13 10:30:23 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Header from './component/Header'
+import { Outlet } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast';
+import { useEffect } from "react";
+import { setDataProduct } from "./redux/productSlide";
+import { useDispatch, useSelector } from "react-redux";
+
+const App = () => {
+  const dispatch = useDispatch()
+  const productData = useSelector((state)=>state.product)
+ 
+  useEffect(()=>{
+    (async()=>{
+      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`)
+      const resData = await res.json()
+      dispatch(setDataProduct(resData))
+    })()
+  },[])
+  return (
+    <>
+        <Toaster />
+    <div>
+      <Header/>
+      <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
+        <Outlet />
+      </main>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
